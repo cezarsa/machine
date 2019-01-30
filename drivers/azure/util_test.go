cdcfd549f9356c99e374a0b7b93d0a464cf3fb5d
Cezar Sa Espinola <cezarsa@gmail.com>
2019-01-30 18:38:17 -0200

    drivers/azure: updates required by new azure lib version

--- drivers/azure/util_test.go
+++ drivers/azure/util_test.go
@@ -3,7 +3,7 @@ package azure
 import (
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/arm/network"
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/network/mgmt/network"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,9 +13,9 @@ func TestParseSecurityRuleProtocol(t *testing.T) {
 		expectedProto network.SecurityRuleProtocol
 		expectedErr   bool
 	}{
-		{"tcp", network.TCP, false},
-		{"udp", network.UDP, false},
-		{"*", network.Asterisk, false},
+		{"tcp", network.SecurityRuleProtocolTCP, false},
+		{"udp", network.SecurityRuleProtocolUDP, false},
+		{"*", network.SecurityRuleProtocolAsterisk, false},
 		{"Invalid", "", true},
 	}
 
